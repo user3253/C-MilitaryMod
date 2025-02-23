dbc33068f3e2488159c4cbd6b9cdc873c1f4bb81
CYC <1606012397@qq.com>
2025-02-23 12:58:54 +0800

    AAA

--- src/content/FUnitTypes.java
+++ src/content/FUnitTypes.java
@@ -17,45 +17,43 @@ public class FUnitTypes {
     public static void load(){
         B2 = new UnitType("B2"){{
             constructor = UnitEntity::create;
-            targetAir = false;
-            maxRange = 200;
-            engineOffset = 14.0F;
-            engineSize = 4f;
-            speed = 5f;
-            accel = 0.005F;
-            drag = 0.01F;
-            circleTarget = true;
-            hitSize = 75f;
-            health = 1000f;
-            baseRotateSpeed = 1.5f;
-            rotateSpeed = 2.5f;
-            armor = 3.5f;
+            health = 340;
+            speed = 1.65f;
+            accel = 0.01f;
+            drag = 0.005f;
             flying = true;
+            hitSize = 75f;
+            targetAir = false;
+            engineOffset = 7.8f;
+            range = 140f;
+            faceTarget = false;
+            armor = 3f;
+            itemCapacity = 0;
             targetFlags = new BlockFlag[]{BlockFlag.factory, null};
-
+            circleTarget = true;
             ammoType = new ItemAmmoType(Items.graphite);
 
             weapons.add(new Weapon(){{
-                minShootVelocity = 0.75f;
-                x = 3f;
-                shootY = 0f;
-                reload = 12f;
-                shootCone = 180f;
-                ejectEffect = Fx.none;
-                inaccuracy = 15f;
-                ignoreRotation = true;
-                shootSound = Sounds.none;
-                bullet = new BombBulletType(27f, 25f){{
-                    width = 10f;
-                    height = 14f;
-                    hitEffect = Fx.flakExplosion;
-                    shootEffect = Fx.none;
-                    smokeEffect = Fx.none;
+                    minShootVelocity = 0.75f;
+                    x = 3f;
+                    shootY = 0f;
+                    reload = 12f;
+                    shootCone = 180f;
+                    ejectEffect = Fx.none;
+                    inaccuracy = 15f;
+                    ignoreRotation = true;
+                    shootSound = Sounds.none;
+                    bullet = new BombBulletType(27f, 25f) {{
+                        width = 10f;
+                        height = 14f;
+                        hitEffect = Fx.flakExplosion;
+                        shootEffect = Fx.none;
+                        smokeEffect = Fx.none;
 
-                    status = StatusEffects.blasted;
-                    statusDuration = 60f;
-                    damage = splashDamage * 0.5f;
-                }};
+                        status = StatusEffects.blasted;
+                        statusDuration = 60f;
+                        damage = splashDamage * 0.5f;
+                    }};
             }});
         }};
 
