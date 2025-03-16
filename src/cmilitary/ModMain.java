9ae8100062d972cb249f0717fe7305ec93aa8e11
CYC <1606012397@qq.com>
2025-03-16 11:52:53 +0800

    upload

--- src/cmilitary/ModMain.java
+++ src/cmilitary/ModMain.java
@@ -1,23 +1,18 @@
 package cmilitary;
 
+
 import cmilitary.content.FFx;
 import cmilitary.content.FSounds;
 import cmilitary.content.FUnitTypes;
 import mindustry.mod.*;
 
 public class ModMain extends Mod{
-    public static Mods.LoadedMod FM;
-
-    public ModMain(){
-
-    }
 
     @Override
     public void loadContent(){
         FFx.load();
         FSounds.load();
         FUnitTypes.load();
-
     }
 
 }
